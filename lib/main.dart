a17d058fdfb42bcf58d9f63adac0d4444b10d9cc
rkasale28 <rohitkasale28@gmail.com>
2019-10-05 17:37:44 +0530

    Marks JSON

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Student st;
-  
-  List icon=[Icons.local_phone,Icons.mail,Icons.home,Icons.school];
+
+  List icon = [Icons.local_phone, Icons.mail, Icons.home, Icons.school];
 
   _HomePageState() {
     loadStudent().then((val) => setState(() {
@@ -191,222 +191,117 @@ class Marks extends StatefulWidget {
 }
 
 class _MarksState extends State<Marks> {
+  List pointers = [];
+
+  _MarksState() {
+    loadStudent().then((val) => setState(() {
+          pointers = val.studentPointer;
+        }));
+  }
+
+  List<DataRow> getRows() {
+    List<DataRow> result = pointers
+        .map(
+          ((element) => DataRow(
+                cells: <DataCell>[
+                  DataCell(Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      element["subject"],
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    ),
+                  )),
+                  DataCell(Container(
+                    width: 100.0,
+                    child: Text(
+                      element["marks"],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    ),
+                  )),
+                ],
+              )),
+        )
+        .toList();
+    result.add(DataRow(cells: [
+      DataCell(Container(
+        width: 170.0,
+        padding: EdgeInsets.only(top: 8.0),
+        decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.white, width: 1.0))),
+        child: Text(
+          "Average",
+          style: TextStyle(fontSize: 14, color: Colors.white),
+        ),
+      )),
+      DataCell(Container(
+          width: 100.0,
+          padding: EdgeInsets.only(top: 8.0),
+          decoration: BoxDecoration(
+              border: Border(top: BorderSide(color: Colors.white, width: 1.0))),
+          child: Text(
+            "9.17",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 14, color: Colors.white),
+          ))),
+    ]));
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
           title: const Text('My Marks'),
           backgroundColor: Color.fromRGBO(12, 104, 24, 4.0),
         ),
         body: Container(
+          color: Colors.black,
           child: Padding(
-            padding: const EdgeInsets.only(top: 14.0),
-            child: Table(
-              columnWidths: <int, TableColumnWidth>{
-                0: FixedColumnWidth(width * 0.67),
-                1: FixedColumnWidth(width * 0.33)
-              },
-              border: TableBorder.all(width: 1.0, color: Colors.green),
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              textDirection: TextDirection.ltr,
-              children: [
-                TableRow(
-                    decoration: new BoxDecoration(
-                        color: Colors.black,
-                        border: Border.all(color: Colors.green, width: 1.0)),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'SUBJECT',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.green[300],
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'POINTERS',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.green[300],
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Applied Mathematics-IV',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '10',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Data Communication And Networking',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '8',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Analysis of Algorithms',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '8',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Computer Organization and Architecture',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '9',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Web Programming',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '10',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Programming Language-II',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '10',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-                TableRow(
-                    decoration: new BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          'Average',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                      TableCell(
-                          child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          '9.17',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                    ]),
-              ],
-            ),
-          ),
+              padding: const EdgeInsets.only(top: 14.0),
+              child: DataTable(
+                  dataRowHeight: 50.0,
+                  columnSpacing: 40.0,
+                  columns: [
+                    DataColumn(
+                        label: Container(
+                      width: 170.0,
+                      padding: EdgeInsets.only(bottom: 8.0),
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ))),
+                      child: Text(
+                        "SUBJECT",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16,
+                            color: Colors.blueAccent),
+                      ),
+                    )),
+                    DataColumn(
+                        label: Container(
+                            width: 100.0,
+                            padding: EdgeInsets.only(bottom: 8.0),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                              color: Colors.white,
+                              width: 1.0,
+                            ))),
+                            child: Text(
+                              "POINTERS",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 16,
+                                  color: Colors.blueAccent),
+                            )))
+                  ],
+                  rows: getRows())),
         ));
   }
 }
--- lib/student_model.dart
+++ lib/student_model.dart
@@ -1,18 +1,22 @@
 class Student{
   String studentName,studentBranch;
   int studentRoll;
-  List studentInfo;
+  List studentInfo,studentPointer,studentAttendance;
   
   Student({
     this.studentName,
     this.studentRoll,
     this.studentBranch,
     this.studentInfo,
+    this.studentPointer,
+    this.studentAttendance
  });
 
 Student.fromJson(Map<String, dynamic> parsedJson):
   studentName= parsedJson['name'],
   studentRoll = parsedJson['rollno'] as int,
   studentBranch = parsedJson ['branch'],
-  studentInfo=parsedJson['info'] as List;
+  studentInfo=parsedJson['info'] as List,
+  studentPointer=parsedJson['pointer'] as List,
+  studentAttendance=parsedJson['attendance'] as List;
 }
